895d19e16b33ae17f03fefdbffccf5b8584ffae8
Antony74 <akb@akb.me.uk>
2016-04-14 22:52:16 +0100

    Got the solution working.

--- generate.r
+++ generate.r
@@ -1,10 +1,18 @@
-#
-# usage: rscript generate.r
-#
 
-source('utils.r');
+poisson = function(x, lambda)
+{
+	# Only do it this way for very small values of x
+	return (  ( exp(-lambda) * (lambda ^ 2) ) / factorial(x)  ); 
+};
 
-lambda = seq(0.1, 10, 0.1);
+bayes = function(likelihoods, priors)
+{
+	lp = likelihoods * priors;
+	evidence = sum(lp);
+	return( lp/evidence );
+};
+
+lambda = seq(0.1, 20, 0.1);
 N = length(lambda);
 
 likelihood = poisson(2, lambda);
@@ -12,4 +20,12 @@ priors = rep(1/N, times=N);
 posterior = bayes(likelihood, priors);
 
 plot(lambda, posterior, type="o", xlab="lambda", ylab="P(lambda|X=2)");
+
+p = function(t)
+{
+	possibities = exp(-lambda*t);
+	return(sum(possibities*posterior));
+};
+
+cat( paste('p(1.72)=', p(1.72), '\n'));
 
