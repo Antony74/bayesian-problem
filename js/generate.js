ee1748275a7c92bfe5839ce4faedd0dd89418c21
Antony <akb@akb.me.uk>
2016-05-08 19:15:20 +0100

    Get the style more like the R code.  I haven't defined a seq function because it's more readable inline... otherwise I think that's about as close as one can get in a language where you can't override operators.

--- js/generate.js
+++ js/generate.js
@@ -2,23 +2,23 @@
 var ndarray = require('ndarray');
 var ops = require('ndarray-ops');
 
-// Choose a sample of possible lambda
-var lambda = [];
-for (var n = 0.1; n <= 20; n += 0.1)
+function wrap(sFunctionName)
 {
-	lambda.push(n);
+	return function(a, b)
+	{
+		var retval = ndarray(Array(a.shape[0]));
+		ops[sFunctionName](retval, a, b);
+		return retval;
+	};
 }
 
-var N = lambda.length;
-lambda = ndarray(lambda);
-
-// Provision some additional rows of registers for storing intermediate calculations
-var priors = ndarray(Array(N));
-var likelihoods = ndarray(Array(N));
-var posterior = ndarray(Array(N));
-var r1 = ndarray(Array(N));
-var r2 = ndarray(Array(N));
-var r3 = ndarray(Array(N));
+// Define various wrappers
+var divs = wrap('divs');
+var mul  = wrap('mul' );
+var muls = wrap('muls');
+var exp  = wrap('exp' );
+var neg  = wrap('neg' );
+var pows = wrap('pows');
 
 function factorial(x)
 {
@@ -31,35 +31,48 @@ function factorial(x)
 	return f;
 }
 
-function possion(poss, x, lambda)
+function possion(x, lambda)
 {
 	// Only do it this way for very small values of x
-	ops.neg(r1, lambda);
-	ops.expeq(r1);
-	ops.pows(r2, lambda, x);
-	ops.mul(r3, r1, r2);
-	ops.divs(poss, r3, factorial(x));
+	return divs(
+				mul( exp( neg(lambda)),
+				     pows(lambda, x)),
+				factorial(x));
 }
 
-function bayes(posterior, likelihoods, priors)
+function bayes(likelihoods, priors)
 {
-	ops.mul(r1, likelihoods, priors);
-	var evidence = ops.sum(r1);
-	ops.divs(posterior, r1, evidence);
+	var lp = mul(likelihoods, priors);
+	var evidence = ops.sum(lp);
+	return divs(lp, evidence);
 }
 
+// Choose a sample of possible lambda
+var lambda = [];
+for (var n = 0.1; n <= 20; n += 0.1)
+{
+	lambda.push(n);
+}
+
+var N = lambda.length;
+lambda = ndarray(lambda);
+
+// Choose some priors
+var priors = ndarray(Array(N));
 ops.assigns(priors, 1/N);
-possion(likelihoods, 2, lambda);
-bayes(posterior, likelihoods, priors);
+
+// Calculate likelihoods
+var likelihoods = possion(2, lambda);
+
+// Calculate posteriors
+var posteriors = bayes(likelihoods, priors);
 
 function p(t)
 {
-	ops.neg(r1, lambda);
-	ops.muls(r2, r1, t);
-	ops.expeq(r2);
-	ops.mul(r3, r2, posterior);
-	return ops.sum(r3);
+	var possibities = exp(neg(muls(lambda, t)));
+	return ops.sum(mul(possibities, posteriors));
 }
 
 console.log('p(1.72)=' + p(1.72));
+
 
