e01ef5de8bc8bc938b45f5a4e5206595650e3439
Antony <akb@akb.me.uk>
2016-06-13 13:46:12 +0100

    Clean up my graphing a little bit - y-axis labels with expressions don't come out very clearly so put the information in the main heading instead.

--- generate.r
+++ generate.r
@@ -31,21 +31,23 @@ cat( paste('p(1.72)=', p(1.72), '\n'));
 # Calculation complete, let's graph it
 #
 
+#png("c:\\temp\\bayesplot.png", 640, 900, type = "cairo-png", pointsize=16);
+
 par(mfrow=c(4,1)); # four rows and one column of graphs
 xlab = expression(lambda);
 
-ylab = expression(paste("P(", lambda, ")"));
-plot(c(0,20), c(0,0.03), type="n", main="Prior", xlab=xlab, ylab=ylab);
+main = expression(paste("Prior, P(", lambda, ")"));
+plot(c(0,20), c(0,0.03), type="n", main=main, xlab=xlab, ylab="");
 grid();
 lines( lambda, prior, col="blue", lwd=2);
 
-ylab = expression(paste("P(X=2|", lambda, ")"));
-plot(c(0,20), c(0,0.3), type="n", main="Likelihood", xlab=xlab, ylab=ylab);
+main = expression(paste("Likelihood, P(X=2|", lambda, ")"));
+plot(c(0,20), c(0,0.3), type="n", main=main, xlab=xlab, ylab="");
 grid();
 lines(lambda, likelihood, col="red", lwd=2);
 
-ylab = expression(paste("P(", lambda, "|X=2)"));
-plot(c(0,20), c(0,0.03), type="n", main="Posterior", xlab=xlab, ylab=ylab);
+main = expression(paste("Posterior, P(", lambda, "|X=2)"));
+plot(c(0,20), c(0,0.03), type="n", main=main, xlab=xlab, ylab="");
 grid();
 lines(lambda, posterior, col="blue", lwd=2);
 
@@ -56,7 +58,10 @@ for (t in T)
 	pt = c(pt, p(t));
 }
 
-plot(T, pt, type="l", xlab="t", ylab="P(X=0)", main="Chance of no failure goes under 5% just before t=1.72", col="red", lwd=2);
+main = expression(paste("Chance of no failure goes under 5% just before t=1.72"));
+plot(T, pt, type="l", xlab="t", ylab="", main=main, col="red", lwd=2);
 grid();
 lines(c(1.72, 1.72), c(-1, 1), col="dark green");
 lines(c(0, 5), c(0.05, 0.05), col="dark green");
+
+#dev.off();
